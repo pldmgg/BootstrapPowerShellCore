74b78b4fedda7fbc0b27ba6cc6d9403552d4369d
pldmgg <pldmgg@mykolab.com>
2018-10-08 00:09:22 -0400

    Various updates

--- BootstrapPowerShellCore/Private/brewinstall.rb
+++ BootstrapPowerShellCore/Private/brewinstall.rb
@@ -1,5 +1,7 @@
 #!/System/Library/Frameworks/Ruby.framework/Versions/Current/usr/bin/ruby
 
+# CREDIT TO: https://gist.githubusercontent.com/skyl/36563a5be809e54dc139/raw/ad509acb9a3accc6408e184ec5e577657bdae7b3/install.rb
+
 argsarray = ARGV
 
 # SET YOUR_HOME TO THE ABSOLUTE PATH OF YOUR HOME DIRECTORY
--- BootstrapPowerShellCore/Public/Bootstrap-PowerShellCore.ps1
+++ BootstrapPowerShellCore/Public/Bootstrap-PowerShellCore.ps1
@@ -785,7 +785,8 @@ function Bootstrap-PowerShellCore {
         brew cask reinstall powershell
     '@
     #>
-    $BrewInstallNoSudoUrl = 'https://gist.githubusercontent.com/skyl/36563a5be809e54dc139/raw/ad509acb9a3accc6408e184ec5e577657bdae7b3/install.rb'
+    #$BrewInstallNoSudoUrl = 'https://gist.githubusercontent.com/skyl/36563a5be809e54dc139/raw/ad509acb9a3accc6408e184ec5e577657bdae7b3/install.rb'
+    $BrewInstallNoSudoUrl = 'https://raw.githubusercontent.com/pldmgg/BootstrapPowerShellCore/master/BootstrapPowerShellCore/Private/brewinstall.rb'
     $MacOSPMInstallScriptPrep = @(
         'usrlocaldir=$(echo \"\"$HOME/usr/local\"\")'
         'if [ ! -d \"\"$usrlocaldir/Cellar\"\" ]; then mkdir -p \"\"$usrlocaldir/Cellar\"\"; fi'
